6ea80ed0882211ad4a94528eb453db1ca306d2e0
nazmul910 <islamnasir910@gmail.com>
2026-04-17 23:55:56 +0600

    Home page done

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function HomeBottomText() {
   return (
-    <div className='font-[font2] flex items-center text-white justify-center gap-2 '>
-      <Link className='text-[6.5vw] leading-[6vw] border-2 border-white flex justify-center items-center rounded-full px-10  pt-5 uppercase'>Projects</Link>
-      <Link  className='text-[6.5vw] leading-[6vw] border-2 border-white flex justify-center items-center rounded-full px-10 pt-5 uppercase'>Agency</Link>   
+    <div className="font-[font2] flex items-center text-white justify-center gap-2 ">
+      <div className="border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] h-44 flex items-center px-14 border-white rounded-full uppercase">
+        <Link className="text-[6vw] mt-6" to="/projects">
+          Projects
+        </Link>
+      </div>
+      <div className="border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] h-44 flex items-center px-14 border-white rounded-full uppercase">
+        <Link className="text-[6vw] mt-6" to="/agency">
+          Agency
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomeBottomText
+export default HomeBottomText;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
       <div className="h-screen w-screen fixed top-0 left-0 right-0">
         <Video />
       </div>
-      <div className="h-screen w-screen relative flex flex-col justify-between">
+      <div className="h-screen w-screen relative pb-5 overflow-hidden flex flex-col justify-between">
         <HeroTopText />
         <HeroBottomText />
       </div>
