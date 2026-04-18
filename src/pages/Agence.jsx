6c91bcde8a038f3b0314fa3508b46dacb7cde58b
nazmul910 <islamnasir910@gmail.com>
2026-04-19 00:32:03 +0600

    agency hero image animation added

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,18 +1,49 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import React, { use } from "react";
+import { ScrollTrigger } from "gsap/all";
+import React, {useRef } from "react";
 
 function Agence() {
 
-  const imageDivRef = use
+  gsap.registerPlugin(ScrollTrigger);
+
+  const imageDivRef = useRef(null);
+
+  const imageRef = useRef(null);
+
+  const imageArray= [
+    "https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7",
+    "https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64",
+    "https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991",
+    "https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2",
+    "https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6",
+    "https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a",
+    "https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847",
+    "https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6",
+    ""
+  ]
+
     useGSAP(function(){
-      gsap.from
+      gsap.to(imageDivRef.current,{
+        scrollTrigger:{
+          trigger:imageDivRef.current,
+          start:"top 30%",
+          end:"top -70%",
+          markers:true,
+          scrub:true,
+          pin:true,
+          onUpdate: (elem) => {
+            const imageIndex = Math.floor(elem.progress * (imageArray.length));
+            imageRef.current.src = imageArray[imageIndex]
+          }
+        }
+      })
     })
   return (
     <div>
       <div className="section1">
         <div ref={imageDivRef} className=" absolute overflow-hidden rounded-3xl w-[15vw] h-[20vw] top-96 left-[30vw] bg-red-500">
-          <img
+          <img ref={imageRef}
             className="object-cover h-full w-full"
             src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7"
             alt=""
