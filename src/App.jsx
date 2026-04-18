3637f6b3627476e608b5688f226d15b1565d4ea6
nazmul910 <islamnasir910@gmail.com>
2026-04-18 21:53:07 +0600

    Fix route path for Agence component and enhance Agence page layout with new design elements

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/agence' element={<Agence />} />
+        <Route path='/agency' element={<Agence />} />
         <Route path='/projects' element={<Projects />} />
       </Routes>
     </div>
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
 
 function Agence() {
   return (
     <div>
-      <h1 className='text-red-400'>Agence1</h1>
+      <div className="section1">
+        <div className=" absolute overflow-hidden rounded-3xl w-[15vw] h-[20vw] top-96 left-[30vw] bg-red-500">
+          <img
+            className="object-cover h-full w-full"
+            src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7"
+            alt=""
+          />
+        </div>
+        <div className="font-[font2] relative text-white">
+          <div className="mt-[55vh]">
+            <h1 className="text-[20vw] text-center uppercase leading-[18vw]">
+              Soixan7e <br /> Douze
+            </h1>
+          </div>
+          <div className="pl-[40%]">
+            <p className="text-6xl ">
+              {" "}
+              &nbsp;
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              Notre curiosité nourrit notre créativité. On reste humbles et on
+              dit non aux gros egos, même le vôtre. Une marque est vivante. Elle
+              a des valeurs, une personnalité, une histoire. Si on oublie ça, on
+              peut faire de bons chiffres à court terme, mais on la tue à long
+              terme. C’est pour ça qu’on s’engage à donner de la perspective,
+              pour bâtir des marques influentes.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="section2 h-screen">
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default Agence
+export default Agence;
