2fe6cc66c2d4eb384bd96ecb707cd2ec74d88415
nazmul910 <islamnasir910@gmail.com>
2026-04-20 23:37:18 +0600

    gsap working

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
+import { useGSAP } from '@gsap/react'
 
 function App() {
+
+  useGSAP(function() {
+    
+  })
+
   return (
     <div className='text-white'>
       <div className='h-screen w-full flex fixed top-0 z-10'>
-        <div className='h-full w-1/5 bg-black'></div>
-        <div className='h-full w-1/5 bg-black'></div>
-        <div className='h-full w-1/5 bg-black'></div>
-        <div className='h-full w-1/5 bg-black'></div>
-        <div className='h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-white'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-white'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
       </div>
       <Routes>
         <Route path='/' element={<Home />} />
