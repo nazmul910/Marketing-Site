1caca99d37d62967bd15ae00904b556b86afda67
nazmul910 <islamnasir910@gmail.com>
2026-04-20 23:39:19 +0600

    Prloader css issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,19 @@ import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 function App() {
 
   useGSAP(function() {
-    
+    gsap.from(".stair",{
+      height:0,
+      duration:2,
+      stagger:{
+        amount:-0.3,
+        
+      }
+    })
   })
 
   return (
