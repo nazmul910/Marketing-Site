394a0f245cea336c21e1f7472a620e2b4535a71a
nazmul910 <islamnasir910@gmail.com>
2026-04-20 23:48:31 +0600

    Complate Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
@@ -8,9 +8,16 @@ import gsap from 'gsap'
 
 function App() {
 
+  const stairParentRef = useRef(null);
+
   useGSAP(function() {
 
     const tl = gsap.timeline();
+
+
+    tl.to(stairParentRef.current,{
+      display:"block"
+    })
 
     tl.from(".stair",{
       height:0,
@@ -26,16 +33,27 @@ function App() {
         amount:-0.25
       }
     })
+
+    tl.to(stairParentRef.current,{
+      display:"none"
+    })
+
+    tl.to(".stair",{
+      y:"0%",
+    })
   })
 
   return (
     <div className='text-white'>
-      <div className='h-screen w-full flex fixed top-0 z-10'>
+
+      <div ref={stairParentRef } className=' h-screen w-full fixed top-0 z-10'>
+        <div className='h-full w-full flex '>
         <div className='stair h-full w-1/5 bg-black'></div>
         <div className='stair h-full w-1/5 bg-black'></div>
         <div className='stair h-full w-1/5 bg-black'></div>
         <div className='stair h-full w-1/5 bg-black'></div>
         <div className='stair h-full w-1/5 bg-black'></div>
+      </div>      
       </div>
       <Routes>
         <Route path='/' element={<Home />} />
