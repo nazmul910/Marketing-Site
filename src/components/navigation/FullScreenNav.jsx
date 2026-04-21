a312fd1a1369f7498152ab821fd7980434a4ce13
nazmul910 <islamnasir910@gmail.com>
2026-04-22 00:15:14 +0600

    fix: enhance structure and styling of FullScreenNav component

--- src/components/navigation/FullScreenNav.jsx
+++ src/components/navigation/FullScreenNav.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React from "react";
 
 function FullScreenNav() {
   return (
-    <div>
-      <div>
-        <div>
-            <h1>Projects</h1>
+    <div id="fullscreennav" className="h-screen w-full absolute bg-amber-900">
+      <div id="all-links">
+        <div className="link">
+          <h1>Projects</h1>
+          <div>
             <div>
-                <h2>Pour Tout Voir</h2>
-                <img src="https://k72.ca/images/teamMembers/Michele_640x290.jpg?w=640&h=290&s=fc2d5857a514aaf26bec2eb052a2d734" alt="" />
-                <h2>Pour Tout Voir</h2>
-                <img src="https://k72.ca/images/blog/blogImg/K72_article_ChatGPT_blogue.jpg?w=640&h=290&s=cec2aa341c22369e36e602c558c49e2a" alt="" />
+              <h2>Pour Tout Voir</h2>
+              <img
+                src="https://k72.ca/images/teamMembers/Michele_640x290.jpg?w=640&h=290&s=fc2d5857a514aaf26bec2eb052a2d734"
+                alt=""
+              />
+              <h2>Pour Tout Voir</h2>
+              <img
+                src="https://k72.ca/images/blog/blogImg/K72_article_ChatGPT_blogue.jpg?w=640&h=290&s=cec2aa341c22369e36e602c558c49e2a"
+                alt=""
+              />
             </div>
+            <div>
+              <h2>Pour Tout Voir</h2>
+              <img
+                src="https://k72.ca/images/teamMembers/Michele_640x290.jpg?w=640&h=290&s=fc2d5857a514aaf26bec2eb052a2d734"
+                alt=""
+              />
+              <h2>Pour Tout Voir</h2>
+              <img
+                src="https://k72.ca/images/blog/blogImg/K72_article_ChatGPT_blogue.jpg?w=640&h=290&s=cec2aa341c22369e36e602c558c49e2a"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default FullScreenNav
+export default FullScreenNav;
