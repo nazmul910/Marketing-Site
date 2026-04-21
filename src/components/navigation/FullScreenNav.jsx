b9085e9700a27d6b154d1871c0ebd76d0d0b5d75
nazmul910 <islamnasir910@gmail.com>
2026-04-22 00:12:10 +0600

    fix: restructure FullScreenNav component layout

--- src/components/navigation/FullScreenNav.jsx
+++ src/components/navigation/FullScreenNav.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 function FullScreenNav() {
   return (
     <div>
-      
+      <div>
+        <div>
+            <h1>Projects</h1>
+            <div>
+                <h2>Pour Tout Voir</h2>
+                <img src="https://k72.ca/images/teamMembers/Michele_640x290.jpg?w=640&h=290&s=fc2d5857a514aaf26bec2eb052a2d734" alt="" />
+                <h2>Pour Tout Voir</h2>
+                <img src="https://k72.ca/images/blog/blogImg/K72_article_ChatGPT_blogue.jpg?w=640&h=290&s=cec2aa341c22369e36e602c558c49e2a" alt="" />
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
