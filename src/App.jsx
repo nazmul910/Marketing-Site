be9a04c19367ca35c36060bf9201ef1d1fed63ea
nazmul910 <islamnasir910@gmail.com>
2026-04-22 21:27:13 +0600

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 import Navbar from './components/navigation/Navbar'
+import FullScreenNav from './components/navigation/FullScreenNav'
 
 function App() {
 
@@ -12,11 +13,12 @@ function App() {
   return (
     <div className='text-black'>
       <Navbar/>
-      <Routes>
+      <FullScreenNav/>
+      {/* <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/agency' element={<Agence />} />
         <Route path='/projects' element={<Projects />} />
-      </Routes>
+      </Routes> */}
     </div>
   )
 }
