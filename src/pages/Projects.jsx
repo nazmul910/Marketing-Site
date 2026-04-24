893fced474c591484ce87eb28d66ebec07fc35a0
nazmul910 <islamnasir910@gmail.com>
2026-04-24 22:38:53 +0600

    without component fix

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,6 +6,16 @@ function Projects() {
       <div className='bg-red-50 pt-[45vh]'>
         <h2 className='font-[font2] text-[9vw] uppercase'>Projets</h2>
       </div>
+      <div>
+        <div className='w-full gap-4 flex mb-4  h-175 bg-amber-400 -mt-20'>
+          <div className='w-1/2 h-full bg-green-900'></div>
+          <div className='w-1/2 h-full bg-blue-900'></div>
+        </div>
+         <div className='w-full gap-4 flex mb-4  h-175 bg-amber-400 '>
+          <div className='w-1/2 h-full bg-green-900'></div>
+          <div className='w-1/2 h-full bg-blue-900'></div>
+        </div>
+      </div>
     </div>
   )
 }
