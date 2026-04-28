ef5d9bb4a5e18ef3b8e7b28f79bc02384e37d2d0
nazmul910 <islamnasir910@gmail.com>
2026-04-28 21:43:28 +0600

    refactor: clean up Projects component layout and remove unnecessary styles

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,13 +3,16 @@ import React from 'react'
 function Projects() {
   return (
     <div className='p-4'>
-      <div className='bg-red-50 pt-[45vh]'>
+      <div className='pt-[45vh]'>
         <h2 className='font-[font2] text-[9vw] uppercase'>Projets</h2>
       </div>
-      <div>
-        <div className='w-full gap-4 flex mb-4  h-175 bg-amber-400 -mt-20'>
-          <div className='w-1/2 h-full bg-green-900'>
-            <img src="public/project1.png" alt="" />
+      <div className='-mt-20'>
+        <div className='w-full gap-4 flex mb-4  h-175 '>
+          <div className='w-1/2 relative h-full hover:rounded-4xl overflow-hidden transition-all duration-300 '>
+            <img className='h-full w-full object-cover' src="public/project1.png" alt="" />
+            <div className=' absolute top-0 left-0 flex items-center justify-center h-full w-full bg-black/10'>
+              <h2 className='uppercase text-3xl font-[font1] text-white border border-white px-2 pt-2 rounded-full '>Voir le projet</h2>
+            </div>
           </div>
 
         </div>
