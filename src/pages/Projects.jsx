e6e35a7d49c0a7b14ae13b8948cafd548e39f85f
nazmul910 <islamnasir910@gmail.com>
2026-04-29 23:33:11 +0600

    feat: implement dynamic rendering of ProjectCard components

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import ProjectCard from '../components/projects/ProjectCard'
 
+
 function Projects() {
+
+  const projects = [{
+    image1: "public/project1.png",
+    image2: "public/project2.png",
+  },{
+    image1: "public/project3.png",
+    image2: "public/project4.png",
+  }]
+
   return (
     <div className='p-4'>
       <div className='pt-[45vh]'>
         <h2 className='font-[font2] text-[9vw] uppercase'>Projets</h2>
       </div>
       <div className='-mt-20'>
-        <ProjectCard/>
+        {
+          projects.map(function(elem){
+            return<ProjectCard key={elem.image1} image1={elem.image1} image2={elem.image2}/>
+          })
+        }
       </div>
     </div>
   )
