28a42446466fbf1c5ceb13acfef134ea203883e0
nazmul910 <islamnasir910@gmail.com>
2026-04-30 22:43:13 +0600

    project page gsap added

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -2,34 +2,32 @@ import React from "react";
 
 function ProjectCard(props) {
   return (
-    <div>
-      <div className="w-full gap-4 flex mb-4  h-175 ">
-        <div className="w-1/2 relative h-full hover:rounded-4xl overflow-hidden transition-all duration-300 ">
-          <img
-            className="h-full w-full object-cover"
-            src={props?.image1}
-            alt=""
-          />
-          <div className=" absolute group top-0 left-0 flex items-center justify-center h-full w-full bg-black/10">
-            <h2 className="uppercase group-hover:opacity-100 transition-all duration-300 opacity-0 text-4xl font-[font1] border-2 text-white border-white px-5 pt-2 rounded-full ">
-              Voir le projet
-            </h2>
-          </div>
-        </div>
-        <div className="w-1/2 relative h-full hover:rounded-4xl overflow-hidden transition-all duration-300">
-          <img
-            className="h-full w-full object-cover"
-            src={props?.image2}
-            alt=""
-          />
-          <div className=" absolute group top-0 left-0 flex items-center justify-center h-full w-full bg-black/10">
-            <h2 className="uppercase group-hover:opacity-100 transition-all duration-300 opacity-0 text-4xl font-[font1] border-2 text-white border-white px-5 pt-2 rounded-full ">
-              Voir le projet
-            </h2>
-          </div>
+    <>
+      <div className="w-1/2 relative h-full hover:rounded-4xl overflow-hidden transition-all duration-300 ">
+        <img
+          className="h-full w-full object-cover"
+          src={props?.image1}
+          alt=""
+        />
+        <div className=" absolute group top-0 left-0 flex items-center justify-center h-full w-full bg-black/10">
+          <h2 className="uppercase group-hover:opacity-100 transition-all duration-300 opacity-0 text-4xl font-[font1] border-2 text-white border-white px-5 pt-2 rounded-full ">
+            Voir le projet
+          </h2>
         </div>
       </div>
-    </div>
+      <div className="w-1/2 relative h-full hover:rounded-4xl overflow-hidden transition-all duration-300">
+        <img
+          className="h-full w-full object-cover"
+          src={props?.image2}
+          alt=""
+        />
+        <div className=" absolute group top-0 left-0 flex items-center justify-center h-full w-full bg-black/10">
+          <h2 className="uppercase group-hover:opacity-100 transition-all duration-300 opacity-0 text-4xl font-[font1] border-2 text-white border-white px-5 pt-2 rounded-full ">
+            Voir le projet
+          </h2>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ProjectCard from '../components/projects/ProjectCard'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 
 
 function Projects() {
@@ -23,16 +26,36 @@ function Projects() {
 
 ]
 
+gsap.registerPlugin(ScrollTrigger)
+
+useGSAP(function(){
+  gsap.from(".hero",{
+    height:"100px",
+    stagger:{
+      amount:0.4
+    },
+    scrollTrigger:{
+      trigger:".lol",
+      start:"top 100%",
+      end:"top -150%",
+      scrub:true,
+    }
+  })
+})
+
   return (
     <div className='p-4'>
       <div className='pt-[45vh]'>
         <h2 className='font-[font2] text-[9vw] uppercase'>Projets</h2>
         
       </div>
-      <div className='-mt-20'>
+      <div className='-mt-20 lol'>
         {
           projects.map(function(elem){
-            return<ProjectCard key={elem.image1} image1={elem.image1} image2={elem.image2}/>
+            return <div className='hero w-full gap-4 flex mb-4  h-175'>
+                <ProjectCard key={elem.image1} image1={elem.image1} image2={elem.image2}/>            
+              </div>
+
           })
         }
       </div>
