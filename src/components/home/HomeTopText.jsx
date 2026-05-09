4ea262bc8745520f0cbc24fab6e6f8731c9cc960
nazmul910 <islamnasir910@gmail.com>
2026-05-09 21:35:59 +0600

    updated ta font size in hero section

--- src/components/home/HomeTopText.jsx
+++ src/components/home/HomeTopText.jsx
@@ -4,8 +4,8 @@ import Video from "./Video";
 function HomeTopText() {
   return (
     <div className="font-[font1] pt-5 text-center text-white">
-      <div className="text-[9.5vw] uppercase leading-[8vw] flex justify-center items-center">L'étincelle </div>
-      <div className="text-[9.5vw] uppercase leading-[8vw] flex justify-center items-center">
+      <div className="text-[7.5vw] uppercase leading-[8vw] flex justify-center items-center">L'étincelle </div>
+      <div className="text-[7.5vw] uppercase leading-[8vw] flex justify-center items-center">
         qui{" "}
         <div className="h-[8vw] w-[16vw] -mt-3 rounded-full overflow-hidden">
           <Video />
