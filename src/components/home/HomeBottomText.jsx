e50a2c0680001b557df5d265759f29e7b3ac6a40
nazmul910 <islamnasir910@gmail.com>
2026-05-09 21:50:31 +0600

    fix: adjust height styles for buttons in HomeBottomText component

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 function HomeBottomText() {
   return (
     <div className="font-[font2] flex items-center text-white justify-center gap-2 ">
-      <div className="border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-20 xl:h-32 flex items-center px-20 border-white rounded-full uppercase">
+      <div className="border-3 hover:border-[#D3FD50] md:h-16 hover:text-[#D3FD50] lg:h-20 xl:h-32 flex items-center px-20 border-white rounded-full uppercase">
         <Link className="text-[4vw] " to="/projects">
           Projects
         </Link>
       </div>
-      <div className="border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-20 xl:h-32 flex items-center px-20 border-white rounded-full uppercase">
+      <div className="border-3 hover:border-[#D3FD50] md:h-16 hover:text-[#D3FD50] lg:h-20 xl:h-32 flex items-center px-20 border-white rounded-full uppercase">
         <Link className="text-[4vw] " to="/agency">
           Agency
         </Link>
