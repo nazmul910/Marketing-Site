ae170ba34bd3d98dd64c6f5048ed58f662afdeec
nazmul910 <islamnasir910@gmail.com>
2026-05-10 23:34:51 +0600

    fix: update font size styles for improved consistency in HomeTopText component

--- src/components/home/HomeTopText.jsx
+++ src/components/home/HomeTopText.jsx
@@ -4,15 +4,15 @@ import Video from "./Video";
 function HomeTopText() {
   return (
     <div className="font-[font1] pt-5 text-center text-white">
-      <div className="text-[7.5vw] uppercase leading-[8vw] flex justify-center items-center">L'étincelle </div>
-      <div className="text-[7.5vw] uppercase leading-[8vw] flex justify-center items-center">
+      <div className="lg:text-[7.5vw] text-[10vw] uppercase leading-[8vw] flex justify-center items-center">L'étincelle </div>
+      <div className="lg:text-[7.5vw] text-[10vw] uppercase leading-[8vw] flex justify-center items-center">
         qui{" "}
         <div className="h-[8vw] w-[16vw] -mt-3 rounded-full overflow-hidden">
           <Video />
         </div>
         génère
       </div>
-      <div className="text-[7.5vw] uppercase leading-[8vw] flex justify-center items-center">la créativité</div>
+      <div className="lg:text-[7.5vw] text-[10vw] uppercase leading-[8vw] flex justify-center items-center">la créativité</div>
     </div>
   );
 }
