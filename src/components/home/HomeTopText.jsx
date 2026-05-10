7b84553443b707b261e36279c5178bb96d5da24b
nazmul910 <islamnasir910@gmail.com>
2026-05-10 23:48:49 +0600

    fix: adjust padding and leading styles for improved layout in HomeTopText and HomeBottomText components

--- src/components/home/HomeTopText.jsx
+++ src/components/home/HomeTopText.jsx
@@ -3,16 +3,16 @@ import Video from "./Video";
 
 function HomeTopText() {
   return (
-    <div className="font-[font1] pt-5 text-center text-white">
-      <div className="lg:text-[7.5vw] text-[10vw] uppercase leading-[8vw] flex justify-center items-center">L'étincelle </div>
-      <div className="lg:text-[7.5vw] text-[10vw] uppercase leading-[8vw] flex justify-center items-center">
+    <div className="font-[font1] mt-72 lg:mt-0 pt-5 text-center text-white">
+      <div className="lg:text-[7.5vw] text-[10vw] uppercase lg:leading-[8vw] leading-[10vw] flex justify-center items-center">L'étincelle </div>
+      <div className="lg:text-[7.5vw] text-[10vw] uppercase lg:leading-[8vw] leading-[10vw] flex justify-center items-center">
         qui{" "}
         <div className="h-[8vw] w-[16vw] -mt-3 rounded-full overflow-hidden">
           <Video />
         </div>
         génère
       </div>
-      <div className="lg:text-[7.5vw] text-[10vw] uppercase leading-[8vw] flex justify-center items-center">la créativité</div>
+      <div className="lg:text-[7.5vw] text-[10vw] uppercase lg:leading-[8vw] leading-[10vw] flex justify-center items-center">la créativité</div>
     </div>
   );
 }
