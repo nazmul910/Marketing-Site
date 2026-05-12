ef8227fb257474a197e9e08a39f1896b7350effa
nazmul910 <islamnasir910@gmail.com>
2026-05-12 23:51:22 +0600

    fix: update text size for improved layout in project and agency links

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -8,12 +8,12 @@ function HomeBottomText() {
         &nbsp; K72 est une agence qui pense chaque action pour nourrir la marque. Demain, dans 5 mois et dans 5 ans. On cherche la friction qui crée l’étincelle pour générer de l’émotion. Pour assurer une relation honnête, on est sans filtre, on dit ce qui doit être dit, on fait ce qui doit être fait.
       </p>
       <div className="border-2 lg:border-3 hover:border-[#D3FD50] md:h-12 hover:text-[#D3FD50] lg:h-20 xl:h-32 flex items-center pt-1 px-5 md:px-5 border-white rounded-full uppercase">
-        <Link className="text-[4vw] " to="/projects">
+        <Link className="text-[4vw] xl:text-[6vw] xl:pt-3 " to="/projects">
           Projects
         </Link>
       </div>
       <div className="border-2 lg:border-3 hover:border-[#D3FD50] md:h-12 hover:text-[#D3FD50] lg:h-20 xl:h-32 flex items-center pt-1 px-5 md:px-5 border-white rounded-full uppercase">
-        <Link className="text-[4vw] " to="/agency">
+        <Link className="text-[4vw] xl:text-[6vw] xl:pt-3 " to="/agency">
           Agency
         </Link>
       </div>
